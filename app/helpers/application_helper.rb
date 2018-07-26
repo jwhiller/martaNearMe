042b3893fec82a5e1f6984574b14222dfa3ed692
James Hiller <hillerj1@gmail.com>
2018-07-26 09:37:13 -0400

    working...no style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+def nearby(mylng, mylat, buslng, buslat)
+  if (mylng - buslng).abs <= 0.01 && (mylat - buslat).abs <= 0.01
+    return true
+  else
+    return false
+  end
+end
 end
